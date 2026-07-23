9bed483ba53aaf9b0aa502c9b5ce52ae7db06699
Aaron <dea6rt@bosch.com>
2026-07-23 08:25:54 +0200

    ne url for host

--- src/database/databricks_client.py
+++ src/database/databricks_client.py
@@ -87,7 +87,7 @@ class DatabricksClient:
         fileName = f"temp{shortName}-{measurement_id}.csv"
         volumePath = f"{TableNames.EXCHANGE}/{fileName}"
         # instantiate the Databricks SDK WorkspaceClient with host and token from .env
-        w = WorkspaceClient(host=self.host, token=self.token)
+        w = WorkspaceClient(host=f"https://{self.host}", token=self.token)
         
         try:
             #create csv
